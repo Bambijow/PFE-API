ea5ed07ec219238346e327e5cdeb21f91804ea86
Romain Dieu <romain.dieu@peoplbrain.com>
2022-03-04 15:24:00 +0100

    feat(users): Added get user profile
    - Added get user profile endpoint

--- src/users/get-users.decorator.ts
+++ src/users/get-users.decorator.ts
@@ -4,6 +4,8 @@ import { Users } from './users.entity';
 export const GetUser = createParamDecorator(
   (_data, ctx: ExecutionContext): Users => {
     const req = ctx.switchToHttp().getRequest();
+    req.user.salt = undefined;
+    req.user.password = undefined;
     return req.user;
   },
 );
